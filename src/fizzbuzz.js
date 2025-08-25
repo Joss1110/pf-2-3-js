7e641d3398dd047a939c206fdf3cc0004cad6101
Jose Angel <josslozano588@gmail.com>
2025-08-25 11:57:57 -0600

    Update fizzbuzz.js
    Ejercicio de fizzbuzz Jose Anegel

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,16 @@
 export function fizzbuzz(){
-  //tu código va aqui
+  //Jose Angel Lozano: Ejerercicio Fizz y Buzz
+
+for (let i = 1; i <= 50; i++) {
+  if (i % 3 = 0 && i % 5 = 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 = 0) {
+    console.log("Fizz");
+  } else if (i % 5 = 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
   
 }
